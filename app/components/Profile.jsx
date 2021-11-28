8e0c1945e6a5e42fb7b27abe3777bb174f4f2ce0
hbrao <hanu.16aug@outlook.com>
2021-11-27 16:46:49 -0800

    Convert to functional component

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 
-export default class Profile extends React.Component {
-    render() {
-        let hobbies = this.props.hobbies.map( hobby => {
-            return <li>{hobby}</li>
-        })
+//Functional component
 
-        return(
-            <div>
-                <h3>{this.props.name}</h3>
-                <p>{this.props.name} is {this.props.age} and {this.props.bio}</p>
-                <h3>Hobbies</h3>
-                <ul>
-                    {hobbies}
-                </ul>
-            </div>
-        )
-    }
-}
+let Profile = props => (
+    <div>
+        <h3>{props.name}</h3>
+        <p>{props.name} is {props.age} and {props.bio}</p>
+        <h3>Hobbies</h3>
+        <ul>
+            {props.hobbies.map(hobby => <li>{hobby}</li>)}
+        </ul>
+    </div>
+)
+
+export default Profile
