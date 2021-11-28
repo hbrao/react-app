e6fa818bedff581ab2332121ec4620c9f4ed8d81
hbrao <hanu.16aug@outlook.com>
2021-11-28 14:59:24 -0800

    Functional Components demo

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,17 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Profile from './Profile.jsx'
 import AddProfile from './AddProfile.jsx'
+
+//ES6 arrow function and parameter destructuring. 
+const AppTitle = ({name}) => (
+    <div>
+        <h1>Welcome to {name} !</h1>
+    </div>
+)
+
+const Vote = ({option1, option2}) => {
+    const [countOption1, setCountOption1] = useState(0);
+    const [countOption2, setCountOption2] = useState(0);
+
+    let winner;
+    if( countOption1 > countOption2 ) {
+        winner = option1;
+    } else if ( countOption2 > countOption1) {
+        winner = option2;
+    } else {
+        winner = "Its a Tie !";
+    }
+
+    return(
+        <div>
+            <p>Votes for {option1} : {countOption1} </p>
+            <p>Votes for {option2} : {countOption2} </p>
+            <p> Leader : {winner} </p>
+            <div>
+                <button onClick={() => setCountOption1(countOption1 + 1)}>
+                    Vote for { option1 }
+                </button>
+                <button onClick={() => setCountOption2(countOption2 + 1)}>
+                    Vote for { option2 }
+                </button>
+            </div>
+        </div>
+    )
+}
 
 export default class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            profiles : [] 
+        }
+        this.addUser = this.addUser.bind(this)
+    }
+
+    componentDidMount() {
+        //NOTE: this.state must be initialized inside constructor method. 
+        //NOTE: Normally this is the place to write ajax request to fetch the state info from APIs. 
+        this.setState({
             profiles : [
                 {name: 'David', age:30, bio: 'enjoys swimming and biking', hobbies: ['swimming', 'biking']}, 
                 {name: 'Sarah', age:40, bio: 'enjoy long walks on the beach', hobbies: ['gardening', 'games']}
             ]
-        }
-        this.addUser = this.addUser.bind(this)
+        })
     }
 
     addUser(newProfile) {
@@ -34,8 +79,14 @@ export default class App extends React.Component {
 
         return(
             <div>
+                <AppTitle name="React App"/>
                 {profiles}
                 <AddProfile addUser={this.addUser}/>
+
+                <p/>
+                <h1>Functional Components</h1>
+                <p/>
+                <Vote option1="Democrats" option2="Republicans"/>
             </div>
         )
     }
