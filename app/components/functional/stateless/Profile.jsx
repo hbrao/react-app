b6ca991f022ac576bbc65ed36886629d9a3dab2d
hanumantha-banda <hanumantha.banda@workday.com>
2022-12-17 10:54:48 -0800

    Refactoring.

--- app/components/functional/stateless/Profile.jsx
+++ app/components/functional/stateless/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Person = styled.div`
+const PersonDiv = styled.div`
     border-style: dashed;
     border-color: lightblue;
     padding: 10px;
@@ -11,28 +11,28 @@ const Person = styled.div`
 
 //Arrow func, Param Destructuring a.k.a Omit render, return
 let Profile = ({name, age, bio, hobbies}) => (
-    <Person>
+    <PersonDiv>
         <h3>{name}</h3>
         <p>{name} is {age} and {bio}</p>
         <h3>Hobbies</h3>
         <ul>
             {hobbies.map(hobby => <li>{hobby}</li>)}
         </ul>
-    </Person>
+    </PersonDiv>
 )
 
 
 let Profile1 = function(props) {
     const {name, age, bio, hobbies} = props;
     return (
-        <Person>
+        <PersonDiv>
             <h3>{name}</h3>
             <p>{name} is {age} and {bio}</p>
             <h3>Hobbies</h3>
             <ul>
                 {hobbies.map(hobby => <li>{hobby}</li>)}
             </ul>
-        </Person>
+        </PersonDiv>
     )
 }
 
