e464a270865a0822666212d561e808224ba2a8f7
hanumantha-banda <hanumantha.banda@workday.com>
2022-12-28 13:58:09 -0800

    Minor change

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -7,7 +7,7 @@ import styled from 'styled-components'
 
 const ExampleDiv = styled.div`
     display: block;
-    overflow: scroll;
+    overflow: hidden;
     border-style: dashed;
     border-color: grey;
     padding: 10px;
